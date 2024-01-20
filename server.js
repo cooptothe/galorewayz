11b3118332e3f32233cd33393446d281ff26fb14
cooptothe <edward.cooper97@gmail.com>
2024-01-20 13:38:02 -0600

    it was my fault

--- server.js
+++ server.js
@@ -76,30 +76,31 @@ app.get('/getProducts/:productType', async (req, res) => {
       },
       body: JSON.stringify({
         query: `
-        query getProducts {
-          products(first: 10) {
-            edges {
-              node {
-                id
-                title
-                productType
-                description
-                handle
-                media(first: 1) {
-                  edges {
-                    node {
-                      previewImage {
-                        url
+          query getProducts($productType: String!) {
+            products(first: 10, query: $productType) {
+              edges {
+                node {
+                  id
+                  title
+                  productType
+                  description
+                  handle
+                  media(first: 1) {
+                    edges {
+                      node {
+                        previewImage {
+                          url
+                        }
                       }
                     }
                   }
-                }
-                variants(first: 1) {
-                  edges {
-                    node {
-                      price {
-                        amount
-                        currencyCode
+                  variants(first: 1) {
+                    edges {
+                      node {
+                        price {
+                          amount
+                          currencyCode
+                        }
                       }
                     }
                   }
@@ -107,70 +108,9 @@ app.get('/getProducts/:productType', async (req, res) => {
               }
             }
           }
-        }
-      `,
+        `,
         variables: {
           productType,
-        },
-      }),
-    });
-
-    const data = await response.json();
-    res.json(data);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-app.get('/getProducts/:handle', async (req, res) => {
-  const { handle } = req.params;
-  console.log(`Request received at /getProducts for product type: ${handle}`);
-
-  try {
-    const response = await fetch('https://galorewayzlifestyle.com/api/2023-01/graphql.json', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Shopify-Storefront-Access-Token': '5fabebbde54d675f57255be67d7f17da',
-      },
-      body: JSON.stringify({
-        query: `
-        query getProducts {
-          products(first: 10) {
-            edges {
-              node {
-                id
-                title
-                productType
-                description
-                handle
-                media(first: 1) {
-                  edges {
-                    node {
-                      previewImage {
-                        url
-                      }
-                    }
-                  }
-                }
-                variants(first: 1) {
-                  edges {
-                    node {
-                      price {
-                        amount
-                        currencyCode
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      `,
-        variables: {
-          handle,
         },
       }),
     });
